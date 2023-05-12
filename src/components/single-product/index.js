37b499e9150e76f0c63a72a99e3943e289be564f
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-13 00:13:26 +0530

    style - single product layout

---
+++ src/components/single-product/index.js
@@ -0,0 +1,32 @@
+import "../single-product/singleProduct.css";
+
+const SingleProduct = ({ product }) => {
+  console.log(product);
+  return (
+    <div className="product-container">
+      <div className="image">
+        <img src={product.image} />
+      </div>
+      <div>
+        <div className="heading">
+          <h4>{product.title}</h4>
+        </div>
+        <div className="description">
+          <p>{product.description}</p>
+        </div>
+        <div className="price">
+          <p>{product.price}</p>
+        </div>
+        <div className="rating">
+          <p>{product.rating}</p>
+        </div>
+      </div>
+      <div>
+        <button>Add to cart</button>
+        <button>Add to wishlist</button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
