d4bb45d8ca4f23f549b979dfd47233ea8cf6d222
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-13 00:15:54 +0530

    cart layout

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Navbar from "../../components/navbar";
+import SingleProduct from "../../components/single-product";
 
+export const cartDB = [
+  {
+    id: 1,
+    title: "Heading",
+    description: "description",
+    price: 4000,
+    rating: 4.5,
+    image: "https://dummyimage.com/300",
+  },
+  {
+    id: 2,
+    title: "Heading",
+    description: "description",
+    price: 4000,
+    rating: 4.5,
+    image: "https://dummyimage.com/300",
+  },
+];
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="cart-container">
+        <div className="products">
+          {cartDB.map((product) => (
+            <SingleProduct product={product} key={product.id} />
+          ))}
+        </div>
+        <div>checkout section</div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
