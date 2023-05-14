31b836094179ae23f38cde8e1f374f1c6bad2ac0
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-14 17:27:28 +0530

    feat:getCategories

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getCategories } from "../../utils/categories/getCategories";
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getCategories();
+        if (response.status === 200) {
+          const data = await response.json();
+          setCategories(data.categories);
+        }
+      } catch (error) {
+        console.error("cat", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(categories);
+
   return <div>Categories</div>;
 };
 
