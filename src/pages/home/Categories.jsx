764f7fdd97fd414fcccc879e584e0166f8f2dc59
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-14 17:53:20 +0530

    refactor:heading and style

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -23,13 +23,16 @@ const Categories = () => {
   console.log(categories);
 
   return (
-    <div className="category-container">
-      {categories.map((category) => (
-        <div className="category" key={category.id}>
-          <p>{category.categoryName}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <h3>Featured Categories</h3>
+      <div className="category-container">
+        {categories.map((category) => (
+          <div className="category" key={category.id}>
+            <p>{category.categoryName.toUpperCase()}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
