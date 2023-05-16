7a2ff507abf9a16a5e438cf89107110244143c46
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-16 13:45:51 +0530

    feat:Get Category

--- src/pages/productList/filters/components/CategoryFilter.jsx
+++ src/pages/productList/filters/components/CategoryFilter.jsx
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "../../../../utils/categories/getCategories";
+
 export const CategoryFilter = () => {
-  const categories = [
-    "Headphones",
-    "In Ears",
-    "Wireless",
-    "Accessories",
-    "Home Audio",
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getCategories();
+        if (response.status === 200) {
+          const { categories } = await response.json();
+          setCategories(categories);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <p>Category</p>
-      {categories.map((category, key) => (
-        <div>
-          <label key={key}>
+      {categories.map((category) => (
+        <div key={category._id}>
+          <label htmlFor={category.categoryName}>
             <input type="checkbox" />
-            {category}
+            {category.categoryName}
           </label>
         </div>
       ))}
