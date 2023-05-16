4df8ed37ee7db405440d7779f35db9ad6f8935c9
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-16 13:00:11 +0530

    refactor

---
+++ src/pages/productList/ProductList.jsx
@@ -0,0 +1,27 @@
+import "./productList.css";
+
+import Navbar from "../../components/navbar";
+import Filters from "../../components/filters";
+
+import { productDB } from "../../data/dummyDB";
+import ProductCard from "../../components/product-card";
+
+const ProductList = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="products-container">
+        <div>
+          <Filters />
+        </div>
+        <div className="products-list">
+          {productDB.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
