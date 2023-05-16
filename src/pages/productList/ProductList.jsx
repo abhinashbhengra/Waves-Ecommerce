3183c301034fbc51eeaf02675918557b385d4d3d
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-16 22:08:31 +0530

    feat:Filtered Data

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -5,8 +5,13 @@ import { Filters } from "./filters/Filters";
 
 import { productDB } from "../../data/dummyDB";
 import ProductCard from "../../components/product-card";
+import { getFilteredData } from "../../utils/categories/getFilteredData";
+import { useContext } from "react";
+import { FilterContext } from "../../context/FilterContext";
 
 const ProductList = () => {
+  const { filterState } = useContext(FilterContext);
+  const filteredData = getFilteredData(productDB, filterState);
   return (
     <div>
       <Navbar />
@@ -15,7 +20,7 @@ const ProductList = () => {
           <Filters />
         </div>
         <div className="products-list">
-          {productDB.map((product) => (
+          {filteredData.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
         </div>
