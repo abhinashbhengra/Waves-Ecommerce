9da6796233ae8f7c19cbe8acc67dc1f45660f3b7
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-17 17:36:34 +0530

    style:Hero Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,32 @@ import "../home/home.css";
 
 import { Navbar } from "../../components/navbar/Navbar";
 import Categories from "./Categories";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <Navbar />
-      <div className="hero-container">Hero Section</div>
+      <div className="hero-container">
+        <div className="hero-cta">
+          <span className="hero-heading">
+            <p>Flagship Audiophile</p>
+            <p>Headphones</p>
+          </span>
+          <span className="hero-description">
+            <p>Explore the finest full-sized headphones for audiophiles</p>
+          </span>
+          <button className="hero-button" onClick={() => navigate("/products")}>
+            EXPLORE{" "}
+          </button>
+        </div>
+        <img
+          className="hero-image"
+          src="/images/hero2.png"
+          alt="hero-section-image"
+        />
+      </div>
       <Categories />
       <div className="feature-container">
         <div className="feature-left">
