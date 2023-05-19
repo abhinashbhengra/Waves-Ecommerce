7f5d1e1bd27b7fbf1c966ae73b223e32795fbb18
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-20 00:32:59 +0530

    style:navbar fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,20 +25,21 @@ export const Navbar = () => {
       <div className="nav-container">
         <div>
           <Link to="/" onClick={handleFilter}>
-            Waves
+            <img className="logo" src="./images/wave-logo.png" alt="logo" />
           </Link>
         </div>
         <div className="search-container">
-          <label htmlFor="input">
-            <input
-              type="text"
-              name="input"
-              placeholder="Search prouducts..."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-          </label>
-          <button onClick={handleSearch}>search</button>
+          <div>
+            <label htmlFor="input">
+              <input
+                type="text"
+                name="input"
+                placeholder="Search prouducts..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </label>
+          </div>
           <div className="display-search">
             {searchProduct &&
               searchProduct.map((product) => (
@@ -48,14 +49,37 @@ export const Navbar = () => {
               ))}
           </div>
         </div>
-
         <div className="links-contaiiner">
           <div>
             <div className="route-link">
-              <Link to="/products">Products</Link>
-              <Link to="/cart">Cart</Link>
-              <Link to="/wishlist">WishList</Link>
-              <Link to="/profile">Account</Link>
+              <Link to="/products">
+                <img
+                  src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/headphones-svgrepo-com.svg?updatedAt=1684520864475"
+                  alt="products-logo"
+                  className="products-logo"
+                />
+              </Link>
+              <Link to="/cart">
+                <img
+                  className="cart-logo"
+                  src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/cart-shopping-shop-svgrepo-com.svg?updatedAt=1684518260440"
+                  alt="cart"
+                />
+              </Link>
+              <Link to="/wishlist">
+                <img
+                  src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/wii.svg?updatedAt=1684519353432"
+                  alt="wishlist-logo"
+                  className="wishlist-logo"
+                />
+              </Link>
+              <Link to="/profile">
+                <img
+                  src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/user-svgrepo-com__1_.svg?updatedAt=1684520054955"
+                  alt="profile-logo"
+                  className="profile-logo"
+                />
+              </Link>
             </div>
           </div>
         </div>
