1562febf478ef5c65000315abd745fdcb580176d
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-20 12:35:46 +0530

    fix:Sign up page style

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -2,6 +2,7 @@ import "../signup/signup.css";
 import { useContext, useState } from "react";
 import { Navbar } from "../../components/navbar/Navbar";
 import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
   const [user, setUser] = useState({
@@ -13,6 +14,8 @@ export const SignUp = () => {
 
   const { handleSignup } = useContext(AuthContext);
 
+  const navigate = useNavigate();
+
   const handleSignupClick = (e) => {
     e.preventDefault();
     handleSignup(user);
@@ -22,46 +25,57 @@ export const SignUp = () => {
     <>
       <Navbar />
       <div className="signup-main-container">
-        <h1>Signup Page</h1>
-        <form onSubmit={handleSignupClick}>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First name"
-            value={user.firstName}
-            onChange={(e) =>
-              setUser((curr) => ({ ...curr, firstName: e.target.value }))
-            }
-          />
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Last name"
-            value={user.lastName}
-            onChange={(e) =>
-              setUser((curr) => ({ ...curr, lastName: e.target.value }))
-            }
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="E-mail"
-            value={user.email}
-            onChange={(e) =>
-              setUser((curr) => ({ ...curr, email: e.target.value }))
-            }
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={user.password}
-            onChange={(e) =>
-              setUser((curr) => ({ ...curr, password: e.target.value }))
-            }
-          />
-          <button type="submit">Create Account</button>
-        </form>
+        <div className="signup-section">
+          <p className="signup-heading">Sign up</p>
+          <form onSubmit={handleSignupClick} className="signup-form">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First name"
+              value={user.firstName}
+              onChange={(e) =>
+                setUser((curr) => ({ ...curr, firstName: e.target.value }))
+              }
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last name"
+              value={user.lastName}
+              onChange={(e) =>
+                setUser((curr) => ({ ...curr, lastName: e.target.value }))
+              }
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              value={user.email}
+              onChange={(e) =>
+                setUser((curr) => ({ ...curr, email: e.target.value }))
+              }
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) =>
+                setUser((curr) => ({ ...curr, password: e.target.value }))
+              }
+            />
+            <button type="submit" className="signup-button">
+              CREATE ACCOUNT
+            </button>
+
+            <p
+              onClick={() => navigate("/login")}
+              className="signup-login-button"
+            >
+              Login
+            </p>
+          </form>
+        </div>
       </div>
     </>
   );
