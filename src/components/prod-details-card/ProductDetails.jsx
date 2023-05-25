0ad6d97771a7f04fb157d2ad44fd39a4a8009444
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-05-25 11:23:23 +0530

    fix:product details style

--- src/components/prod-details-card/ProductDetails.jsx
+++ src/components/prod-details-card/ProductDetails.jsx
@@ -1,3 +1,5 @@
+import "../prod-details-card/productDetails.css";
+
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { WishlistContext } from "../../context/WishlistContext";
@@ -32,50 +34,72 @@ export const ProductDetails = () => {
     <>
       <Navbar />
       {product && product.image && (
-        <div className="productCard-container">
-          <div className="productCard-wishlist">
-            {wishlistItems.find(({ _id }) => _id === product._id) ? (
-              <div onClick={() => removeFromWishlist(product._id)}>
-                <img src="/images/filledheart.svg" alt="add_to_whislist" />
+        <div className="productDetails-main-container">
+          <div className="productDetails-container">
+            <div className="productDetails-image">
+              <img src={product.image} />
+            </div>
+            <div className="productDetails-section">
+              <div className="productDetails-brand">
+                <p>AUDIO-TECHNICA</p>
               </div>
-            ) : (
-              <div onClick={() => addToWishlist(product)}>
-                <img src="/images/heart.svg" alt="add_to_whislist" />
+              <div className="productDetails-heading">
+                <p>{product.title}</p>
               </div>
-            )}
-          </div>
-          <div className="productCard-image">
-            <img src={product.image} />
-          </div>
-          <div>
-            <div className="productCard-heading">
-              <p>{product.title}</p>
+              <div className="productDetails-description">
+                <p>{product.description}</p>
+              </div>
+              <div className="break"></div>
+              <div className="price-rating-container">
+                <div className="productDetails-price">
+                  <p>₹ {product.price} </p>
+                </div>
+                <div className="productDetails-rating">
+                  <p>{product.rating} </p>
+                  <span>
+                    <img
+                      src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/star-c.png?updatedAt=1684993521625"
+                      alt="star"
+                    />
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                {cartItems.find(({ _id }) => _id === product._id) ? (
+                  <button
+                    onClick={() => navigate("/cart")}
+                    className="prod-cart-button"
+                  >
+                    Go to Cart
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => addToCart(product)}
+                    className="prod-cart-button"
+                  >
+                    Add to Cart
+                  </button>
+                )}
+              </div>
+              <div>
+                {wishlistItems.find(({ _id }) => _id === product._id) ? (
+                  <button
+                    onClick={() => removeFromWishlist(product._id)}
+                    className="prod-wishlist-button"
+                  >
+                    <p>Remove from wishlist</p>
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => addToWishlist(product)}
+                    className="prod-wishlist-button"
+                  >
+                    Add to Wishlist
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="productCard-description">
-              <p>{product.description}</p>
-            </div>
-            <div className="productCard-price">
-              <p>
-                ₹ {product.price} <span className="prev-price">₹ 12399</span>
-              </p>
-            </div>
-            <div className="productCard-rating">
-              <p>{product.rating}</p>
-            </div>
-          </div>
-          <div>
-            {cartItems.find(({ _id }) => _id === product._id) ? (
-              <button onClick={() => navigate("/cart")} className="cart-button">
-                Go to Cart
-              </button>
-            ) : (
-              <button
-                onClick={() => addToCart(product)}
-                className="cart-button"
-              >
-                Add to Cart
-              </button>
-            )}
           </div>
         </div>
       )}
