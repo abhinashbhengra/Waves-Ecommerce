b365617f620dcdc049c2c7f49daa7aa8f694d84c
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-04 10:10:00 +0530

    fix:address management profile section

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,12 +1,19 @@
 import "../user/user.css";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navbar } from "../../components/navbar/Navbar";
 import { AuthContext } from "../../context/AuthContext";
 
 export const User = () => {
   const { authState, handleLogout } = useContext(AuthContext);
   const { token } = authState;
+  const [address, setAddress] = useState();
+  const [tab, setTab] = useState("profile");
+
+  const handleTab = (type) => {
+    // console.log(type);
+    type === "address" ? setTab("address") : setTab("profile");
+  };
 
   useEffect(() => {
     const fetchaddress = async () => {
@@ -20,7 +27,7 @@ export const User = () => {
         });
         const data = await response.json();
 
-        console.log("address", data);
+        console.log("address", data.address[0]);
       } catch (e) {
         console.log(e);
       }
@@ -30,16 +37,52 @@ export const User = () => {
   return (
     <>
       <Navbar />
-      <div className="user-main-container">
-        <div className="user">
-          {token && (
-            <div>
-              <p>Hello! {authState.user.firstName}</p>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          )}
+      <div className="tab-container">
+        <div
+          className="profile-tab"
+          onClick={() => handleTab("profile")}
+          style={{ borderBottom: tab === "profile" ? "2px solid grey" : "" }}
+        >
+          Profile
+        </div>
+        <div
+          className="address-tab"
+          onClick={() => handleTab("address")}
+          style={{ borderBottom: tab !== "profile" ? "2px solid grey" : "" }}
+        >
+          Address
         </div>
       </div>
+      {tab === "profile" ? (
+        <div className="user-main-container">
+          <div className="user">
+            {token && (
+              <div className="profile-section">
+                <div className="profile-heading">
+                  <p>Hello {authState.user.firstName}</p>
+                </div>
+                <div className="profile-details">
+                  <p>First Name : {authState.user.firstName}</p>
+                  <p>Last Name : {authState.user.lastName}</p>
+                  <p>Email : {authState.user.email}</p>
+                  <button onClick={handleLogout}>Logout</button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="user-main-container">
+          <div className="user">
+            {token && (
+              <div>
+                <p>Address {authState.user.firstName}</p>
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
