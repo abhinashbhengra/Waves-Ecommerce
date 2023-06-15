e545e973b4e2616a4db286c53c7ef900773a3263
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-15 15:20:46 +0530

    fix:new launches component

--- src/components/bestSeller_Card/BestSellerCard.jsx
+++ src/components/bestSeller_Card/BestSellerCard.jsx
@@ -14,22 +14,27 @@ export const BestSellerCard = ({ product }) => {
   return (
     <div className="sellerCard-container">
       <div className="productCard-image">
-        <img src={product.image} />
+        <img src={product?.image} />
       </div>
       <div>
         <div className="sellerCard-heading">
-          <p>{product.title}</p>
+          <p>{product?.title}</p>
         </div>
         <div className="sellerCard-description">
-          <p>{product.description}</p>
+          <p>{product?.description}</p>
         </div>
         <div className="sellerCard-price">
           <p>
-            ₹ {product.price} <span className="prev-price">₹ 12399</span>
+            ₹ {product?.price} <span className="prev-price">₹ 12399</span>
           </p>
         </div>
         <div className="sellerCard-rating">
-          <p>{product.rating}</p>
+          <p className="rating-text">{product?.rating}</p>
+          <img
+            src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/star-c.png?updatedAt=1684993521625"
+            alt="star"
+            className="rating-logo"
+          />
         </div>
       </div>
     </div>
--- src/pages/home/new-launch/NewLaunch.jsx
+++ src/pages/home/new-launch/NewLaunch.jsx
@@ -1,19 +1,30 @@
 import "../new-launch/newLaunch.css";
 
 import { BestSellerCard } from "../../../components/bestSeller_Card/BestSellerCard";
-import { useState } from "react";
-
-import { productDB } from "../../../data/dummyDB";
+import { useState, useEffect } from "react";
 
 export const NewLaunch = () => {
-  // const [products, setProducts] = useState([])  // use for mockbee
-  const newLaunch = productDB.filter((product) => product._isNewLaunch);
+  const [products, setProducts] = useState([]);
+  const newLaunch = products.filter((product) => product._isNewLaunch);
   const displayNewLaunch = [
     newLaunch[0],
     newLaunch[1],
     newLaunch[2],
     newLaunch[3],
   ];
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/api/products");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <>
@@ -23,7 +34,7 @@ export const NewLaunch = () => {
         </span>
         <div className="new_launch-products">
           {displayNewLaunch.map((product) => (
-            <div key={product._id}>
+            <div key={product?._id}>
               <BestSellerCard product={product} />
             </div>
           ))}
