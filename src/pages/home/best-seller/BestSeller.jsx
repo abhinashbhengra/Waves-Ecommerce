f97c0cde91457e90885fcff43d2e75f5112bfec0
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-15 15:03:03 +0530

    fix:bestseller component

--- src/pages/home/best-seller/BestSeller.jsx
+++ src/pages/home/best-seller/BestSeller.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../best-seller/bestSeller.css";
-import { productDB } from "../../../data/dummyDB";
 
 export const BestSeller = () => {
-  //   const [products, setProducts] = useState([]); // use for mockbee
-  const bestproducts = productDB.filter((product) => product._isBestSeller);
-  const displayBestProducts = [bestproducts[0], bestproducts[1]];
+  const [products, setProducts] = useState([]);
+  const bestproducts = products.filter((product) => product._isBestSeller);
+  const displayBestProducts = [bestproducts[18], bestproducts[21]];
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/api/products");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <>
@@ -15,16 +27,16 @@ export const BestSeller = () => {
         </span>
         <div className="best-container">
           {displayBestProducts.map((product) => (
-            <div className="best_seller-product" key={product._id}>
+            <div className="best_seller-product" key={product?._id}>
               <div>
                 <img
                   className="best_seller-image"
-                  src={product.image}
+                  src={product?.image}
                   alt="best-seller"
                 />
               </div>
               <div className="best_seller-text">
-                <p>{product.title}</p>
+                <p>{product?.title}</p>
               </div>
             </div>
           ))}
