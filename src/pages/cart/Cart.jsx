5a610c9209fc4e3b42ec1a1ef473406ee34f1a5c
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-15 17:34:10 +0530

    style:fix cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -29,53 +29,60 @@ export const Cart = () => {
       <Navbar />
       <div className="cart-container">
         <p className="cart-main-heading">Cart</p>
-        <div className="cart-product-details">
-          {cartItems.length < 1 ? (
-            <div className="empty-carty-descriptio">
-              <p>Oops! Empty cart.</p>
-              <button onClick={handleExploreClick}>Explore</button>
-            </div>
-          ) : (
+        {cartItems.length < 1 ? (
+          <div className="empty-cart">
+            <p className="">Your cart is empty</p>
+            <button
+              onClick={handleExploreClick}
+              className="continue-shopping-button"
+            >
+              <p>Continue Shopping</p>
+            </button>
+          </div>
+        ) : (
+          <div className="cart-product-details">
             <div>
               {cartItems.map((product) => (
                 <SingleProduct product={product} key={product._id} />
               ))}
             </div>
-          )}
 
-          {cartItems.length > 0 && (
-            <div className="checkout">
-              <p className="price-details-heading">Price Details</p>
-              {cartItems.map((product) => (
-                <div className="p-flex" key={product._id}>
+            {cartItems.length > 0 && (
+              <div className="checkout">
+                <p className="price-details-heading">Price Details</p>
+                {cartItems.map((product) => (
+                  <div className="p-flex" key={product._id}>
+                    <div>
+                      <p className="checkout-subtotal-heading">
+                        {product.title} ({product.qty})
+                      </p>
+                    </div>
+                    <div>
+                      <p className="checkout-subtotal-price">
+                        ₹ {product.price}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+                <div className="break"></div>
+                <div className="p-flex">
                   <div>
-                    <p className="checkout-subtotal-heading">
-                      {product.title} ({product.qty})
-                    </p>
+                    <p className="checkout-total-heading">Total</p>
                   </div>
                   <div>
-                    <p className="checkout-subtotal-price">₹ {product.price}</p>
+                    <p className="checkout-total-price">₹ {total}</p>
                   </div>
                 </div>
-              ))}
-              <div className="break"></div>
-              <div className="p-flex">
-                <div>
-                  <p className="checkout-total-heading">Total</p>
-                </div>
-                <div>
-                  <p className="checkout-total-price">₹ {total}</p>
-                </div>
+                <button
+                  className="checkout-button"
+                  onClick={() => navigate("/checkout")}
+                >
+                  <p>proceed to checkout</p>
+                </button>
               </div>
-              <button
-                className="checkout-button"
-                onClick={() => navigate("/checkout")}
-              >
-                <p>proceed to checkout</p>
-              </button>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
