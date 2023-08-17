4733096a8f2bc74dd0e7fa10daf64221ef1b4674
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-17 14:00:41 +0530

    rating section style update

--- src/pages/productList/filters/components/RatingFilter.jsx
+++ src/pages/productList/filters/components/RatingFilter.jsx
@@ -1,31 +1,77 @@
 export const RatingFilter = ({ rating, filterDispatch }) => {
   const handleRating = (e) => {
-    const value = e.target.value;
-    filterDispatch({ type: "SET_RATING", payload: value });
+    switch (e.target.id) {
+      case "four-star":
+        filterDispatch({ type: "RATING", payload: 4 });
+        break;
+      case "three-star":
+        filterDispatch({ type: "RATING", payload: 3 });
+        break;
+      case "two-star":
+        filterDispatch({ type: "RATING", payload: 2 });
+        break;
+      case "one-star":
+        filterDispatch({ type: "RATING", payload: 1 });
+        break;
+      default:
+        filterDispatch({ type: "RATING", type: null });
+    }
   };
 
   return (
     <>
       <div className="rating-main-container">
         <p className="filter-heading">Rating</p>
-        <p className="rating-range">
-          {rating}{" "}
-          <span>
-            <img
-              src="https://ik.imagekit.io/u6itcrvxy/Nav_Icon/star-c.png?updatedAt=1684993521625"
-              alt="star"
-            />
-          </span>{" "}
-          & above
-        </p>
-        <input
-          type="range"
-          min={2.5}
-          max={4.5}
-          step={0.5}
-          value={rating}
-          onChange={handleRating}
-        />
+        <div className="rating">
+          <div>
+            <label className="rating-label">
+              <input
+                type="radio"
+                name="rating"
+                id="four-start"
+                checked={rating === 4}
+                onChange={handleRating}
+              />
+              <p className="rating-detail">4 stars and above</p>
+            </label>
+          </div>
+          <div>
+            <label className="rating-label">
+              <input
+                type="radio"
+                name="rating"
+                id="three-start"
+                checked={rating === 3}
+                onChange={handleRating}
+              />
+              <p className="rating-detail">3 stars and above</p>
+            </label>
+          </div>
+          <div>
+            <label className="rating-label">
+              <input
+                type="radio"
+                name="rating"
+                id="two-start"
+                checked={rating === 2}
+                onChange={handleRating}
+              />
+              <p className="rating-detail">2 stars and above</p>
+            </label>
+          </div>
+          <div>
+            <label className="rating-label">
+              <input
+                type="radio"
+                name="rating"
+                id="one-start"
+                checked={rating === 1}
+                onChange={handleRating}
+              />
+              <p className="rating-detail">1 stars and above</p>
+            </label>
+          </div>
+        </div>
       </div>
     </>
   );
